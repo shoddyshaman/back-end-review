07b8fc98cbfd394f85aa951a7cea57240645c3f9
shoddyshaman <dhananjoy.tiwari@gmail.com>
2023-05-12 13:11:25 -0700

    finsihed review back-end

--- public/main.js
+++ public/main.js
@@ -7,17 +7,33 @@ const nameInput = document.querySelector('#song-name')
 const artistInput = document.querySelector('#artist-name')
 const categories = document.querySelector('#categories')
 
+const newSong = document.querySelector('#new-song')
+const newArtist = document.querySelector('#new-artist')
+const newCategories = document.querySelector('#new-categories')
+const songUpdateId = document.querySelector('#song-id')
+const editForm = document.querySelector('#edit-form')
+
 const getAllSongs = () => {
     // console.log('I RAN')
 
     axios.get(`${baseURL}/api/songs`)
     .then(res => {
-        console.log(res.data)
+        // console.log(res.data)
         createSongDisplay(res.data)
     })
 }
 
-document.addEventListener('DOMContentLoaded',getAllSongs)
+const deleteSong = (id) => {
+    axios.delete(`${baseURL}/api/song/${id}`)
+    .then(res => {
+        createSongDisplay(res.data)
+    })
+    .catch(err => {
+        alert(err.message)
+        console.log(err)
+    })
+}
+
 
 const createSongCard = (obj) => {
     let card = document.createElement('div')
@@ -26,10 +42,11 @@ const createSongCard = (obj) => {
     <h2>${obj.name}</h4>
     <h4>${obj.artist}</h6>
     <ul>
-      <li>${obj.category[0]}</li>
-      <li>${obj.category[1]}</li>
-      <li>${obj.category[2]}</li>
+    <li>${obj.category[0]}</li>
+    <li>${obj.category[1] || 'N/A'}</li>
+    <li>${obj.category[2] || 'N/A'}</li>
     </ul>
+    <button onclick="deleteSong(${obj.id})">Delete</button>
     <br>
     `
     displaySection.appendChild(card)
@@ -44,7 +61,7 @@ const createSongDisplay = (arr) => {
 
 const formHandler = (e) => {
     e.preventDefault()
-
+    
     // 'pop,rock,grunge' = ['pop','rock','grunge']
     let newCat = categories.value.split(',')
     console.log(newCat)
@@ -59,7 +76,28 @@ const formHandler = (e) => {
     .catch(err => console.log(err))
 }
 
-
+const editFormHandler = (e) => {
+    e.preventDefault()
+    //we will get the info from the inputs on event firing
+    //we are gonna package that info into a body(except for id which will be a param) and then send it with the http request
+    let newCat = newCategories.value.split(',')
+    const body = {
+        name:newSong.value,
+        artist:newArtist.value,
+        category:newCat
+    }
+    //axios request
+    axios.put(`${baseURL}/api/song/${songUpdateId.value}`,body)
+    .then(res => {
+        console.log(res.data)
+        createSongDisplay(res.data)
+    })
+    .catch(err => {
+        alert(err.message)
+    })
+} 
 
 
 form.addEventListener('submit',formHandler)
+editForm.addEventListener('submit',editFormHandler)
+document.addEventListener('DOMContentLoaded',getAllSongs)
--- server/server.js
+++ server/server.js
@@ -61,6 +61,48 @@ app.post('/api/song',(req,res) => {
     }
     songs.push(newSong)
     res.status(200).send(songs)
+    id++
+})
+
+app.put('/api/song/:id',(req,res) => {
+  const { id } = req.params
+  const {name,artist,category} = req.body
+  console.log(category)
+  //locate the element using the id
+  const index = songs.findIndex((song) => {
+    return song.id === +id
+  })
+
+  //findIndex will return the index if it is able to find the element, if not its gonna return -1
+  if(index !== -1){
+    const updatedSong = {
+      id,
+      name,
+      artist,
+      category
+    }
+    //replace the old element with the updated one
+    songs.splice(index,1,updatedSong)
+    res.status(200).send(songs)
+  } else {
+    res.status(400).send('song not found')
+  }
+})
+
+app.delete('/api/song/:id', (req,res) => {
+  const {id} = req.params
+
+  //locate the element in the array
+  const index = songs.findIndex(song => song.id === +id)
+
+  //findIndex will return the index if it is able to find the element, if not its gonna return -1
+  if(index !== -1){
+    songs.splice(index,1)
+    res.status(200).send(songs)
+  }
+  else {
+    res.status(400).send('cannot delete song')
+  }
 })
 
 app.listen(4004, () => console.log(`server is up and running on 4004`));
